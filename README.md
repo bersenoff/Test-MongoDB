3d8c668cf6258013b419eb82d7cd0bd9cd8c9e0c
Nikita Bersenev <bersenoff@bk.ru>
2021-03-10 15:37:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instruction 
+
+1. Clone repository to a local machine
+2. Run command "npm install" in the folder of cloned repository
+3. Run command "npm run start". It will be create a build and will be run application.
+4. Profit
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { MongoClient } from "mongodb";
+
+(async () => {
+  const first = require("../json/first.json");
+  const second = require("../json/second.json");
+
+  const url = "mongodb://localhost:27017";
+  const dbName = "test";
+
+  const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+  try {
+    const client = await mongoClient.connect();
+    const db = client.db(dbName);
+
+    const firstCollection = db.collection("first");
+    const secondCollection = db.collection("second");
+    const thirdCollection = db.collection("third");
+
+    // потом убрать
+    await firstCollection.deleteMany({});
+    await secondCollection.deleteMany({});
+    await thirdCollection.deleteMany({});
+
+    await firstCollection.insertMany(first);
+    await secondCollection.insertMany(second);
+
+    await firstCollection.aggregate([
+      {
+         $set: { 
+           longitude: { $first: "$location.ll" }, 
+           latitude: { $arrayElemAt: ["$location.ll", 1] },
+           amount_students: { $sum: "$students.number" }
+         }
+      },
+      { 
+        $lookup: {
+          from: "second",
+          localField: "country",
+          foreignField: "country",
+          as: "second"
+        },
+      },
+      {
+        $set: {
+          diff: { $subtract: ["$amount_students", { $first: "$second.overallStudents" }] },
+        }
+      },
+      {
+        $group: {
+          _id: "$country",
+          allDiffs: {
+            $push: "$diff"
+          },
+          count: { $sum: 1 },
+          longitude: {
+            $push: "$longitude"
+          },
+          latitude: {
+            $push: "$latitude"
+          },
+        }
+      },
+      {
+        $out: "third"
+      }
+    ]).toArray();
+
+    client.close();
+  } catch (err) {
+    console.error(err);
+  }
+})();
